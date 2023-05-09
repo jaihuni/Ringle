a0d61db7b3fac4d58603ef6629a78e4e8222d1d2
jaihuni <kjhyama@gmail.com>
2023-05-10 04:17:49 +0900

    Add data

---
+++ data.rb
@@ -0,0 +1,101 @@
+# Music
+Music.new(title: "UNFORGIVEN (feat. Nile Rodgers)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "The World is My Oyster (2023 Ver.)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "FEARLESS (2023 ver.)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Blue Flame (2023 ver.)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "The Hydra", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "ANTIFRAGILE", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Impurities", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Burn the Bridge", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "No-Return (Into the unknown)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Eve, Psyche & The Bluebeard's wife", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "FEARNOT (Between you, me and the lamppost)", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Flash Forward", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "Fire in the belly", artist: "LE SSERAFIM", album: "UNFORGIVEN", like: rand(1...100000)).save
+Music.new(title: "I AM", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Blue Blood", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Kitsch", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Lips", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Heroine", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Mine", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "섬핏 (Hypnosis)", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "NOT YOUR GIRL", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "궁금해 (Next Page)", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Cherish", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Shine With Me", artist: "IVE", album: "I've IVE", like: rand(1...100000)).save
+Music.new(title: "Love Is An Open Door", artist: "Kristen Bell, Santino Fontana", album: "겨울왕국 OST", like: rand(1...100000)).save
+Music.new(title: "라일락", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "Flu", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "Coin", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "봄 안녕 봄", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "Celebrity", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "돌림노래(Feat. DEAN)", artist: "아이유, DEAN", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "빈 컵 (Empty Cup)", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "아이와 나의 바다", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "어푸 (Ah puh)", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "에필로그", artist: "아이유", album: "IU 5th Album 'LILAC'", like: rand(1...100000)).save
+Music.new(title: "재난의 여명", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "우주 유영", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "쇼타임", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "프리스타일", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "뭇별의 부름", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "SF소설", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "행성 표류", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "위기 감지", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "일촉즉발", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "영혼 깊은 곳", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "몰입감", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "미스터리 영화", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "절체절명", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "얼터너티브 록", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "솔티 문", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "스타레일", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 통제 불능", like: rand(1...100000)).save
+Music.new(title: "잔화", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "화롯불", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "시끌벅적", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "식어 가는 태양", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "서늘한 길", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "질서", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "엄한 돌파", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "따스한 태양", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "단잠", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "조용한 눈", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "매서운 바람", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "조율", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "단단한 얼음", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "흔적없는 눈", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "과거의 그림자", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "지는 서리", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "불씨", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "지하", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "맹염", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "전투 시작", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "얼어붙은 눈물", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "남은 온기", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "짙은 안개", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "대부", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "한파 아래", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "영원한 겨울", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "혹한 행군", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "운명의 길", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "임전무퇴", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "들불", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "들불 - Instrumental", artist: "HOYO-MIX", album: "붕괴: 스타레일 - 잿더미에 녹아내린 눈", like: rand(1...100000)).save
+Music.new(title: "달", artist: "임정희", album: "마녀보감 OST Part.3", like: rand(1...100000)).save
+Music.new(title: "달 (Inst.)", artist: "임정희", album: "마녀보감 OST Part.3", like: rand(1...100000)).save
+Music.new(title: "Clear Morning", artist: "Yui Ogura", album: "Clear Morning", like: rand(1...100000)).save
+Music.new(title: "가습기", artist: "한요한", album: "가습기", like: rand(1...100000)).save
+Music.new(title: "Be Honest (feat. Burna Boy)", artist: "Jorja Smith", album: "Be Honest (feat. Burna Boy)", like: rand(1...100000)).save
+Music.new(title: "Sorush's Purity", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Resonance of Kvarena", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Madiens of Sancitity", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "The Caress of Three Mothers", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Where Simurgh Rests", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Fantasy of Ten Thousand Blossoms", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Dirge of Newborn Life", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Undisturbed Tranquility", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Mirage of the Oasis", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Lonesome Road", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Grief of the Lost", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Dividing Paths", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
+Music.new(title: "Dawnlight Over Dunes", artist: "HOYO-MIX", album: "원신-불가사의한 모래의 언덕", like: rand(1...100000)).save
