7280735a9d01713a2f01c757e3b11ab8f7284e7f
jaihuni <kjhyama@gmail.com>
2023-05-10 20:46:39 +0900

    Fixed typo

--- app/controllers/list_musics_controller.rb
+++ app/controllers/list_musics_controller.rb
@@ -1,12 +1,12 @@
 class ListMusicsController < ApplicationController
   def create
-    @playlist = Playlist.find_by(user_id: params[:user_id], playlist_id: params[:playlist_id])
+    @playlist = Playlist.find_by(user_id: params[:user_id], id: params[:playlist_id])
     @playlist.list_musics.create(list_music_params)
     redirect_to user_playlist_path(@playlist.user_id, @playlist)
   end
 
   def destroy
-    @playlist = Playlist.find(user_id: params[:user_id], params[:playlist_id])
+    @playlist = Playlist.find_by(user_id: params[:user_id], id: params[:playlist_id])
     @music = @playlist.list_musics.find(params[:id])
     @music.destroy
     redirect_to user_playlist_path(@playlist.user_id, @playlist), status: :see_other
